215a8ca2d4bb560607416b214ed79d1020103008
villa <marvinavi24@gmail.com>
2025-04-17 00:36:32 +0300

    first commit

--- build/lib/slowfast/utils/distributed.py
+++ build/lib/slowfast/utils/distributed.py
@@ -10,14 +10,14 @@ import pickle
 import torch
 import torch.distributed as dist
 
-from pytorchvideo.layers.distributed import * #(  # noqa
-#     cat_all_gather,
-#     get_local_process_group,
-#     get_local_rank,
-#     get_local_size,
-#     get_world_size,
-#     init_distributed_training as _init_distributed_training,
-# )
+from pytorchvideo.layers.distributed import (  # noqa
+    cat_all_gather,
+    get_local_process_group,
+    get_local_rank,
+    get_local_size,
+    get_world_size,
+    init_distributed_training as _init_distributed_training,
+)
 
 
 def init_distributed_training(cfg):
